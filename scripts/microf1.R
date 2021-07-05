93582b48cdf365a525232087214e03cf8a34c2ff
Elaine Cecília Gatto <elainececiliagatto@gmail.com>
2021-07-05 16:46:50 -0300

    Update microf1.R

--- scripts/microf1.R
+++ scripts/microf1.R
@@ -204,7 +204,7 @@ print(system(str4))
 ########################################################################################################################
 #cat("\n Copy Results to google drive")
 #origem = paste(diretorios$folderDatasetResults, "/", dataset_name, "-results-bpc.tar.gz", sep="")
-#destino = paste("cloud:elaine/[2021]ResultadosExperimentos/Best-Partition-Clus/R1/Micro-F1/", dataset_name, sep="")
+#destino = paste("cloud:elaine/[2021]ResultadosExperimentos/Best-Partitions/R1/Micro-F1/", dataset_name, sep="")
 #comando1 = paste("rclone -v copy ", origem, " ", destino, sep="")
 #print(system(comando1))
 
@@ -212,7 +212,7 @@ print(system(str4))
 ########################################################################################################################
 #cat("\n Copy Outupt to google drive")
 #origem = diretorios$folderOutputDataset
-#destino = paste("cloud:elaine/[2021]ResultadosExperimentos/Best-Partition-Clus/R1/Micro-F1/", dataset_name, sep="")
+#destino = paste("cloud:elaine/[2021]ResultadosExperimentos/Best-Partitions/R1/Micro-F1"", dataset_name, sep="")
 #comando2 = paste("rclone -v copy ", origem, " ", destino, sep="")
 #print(system(comando2))
 
