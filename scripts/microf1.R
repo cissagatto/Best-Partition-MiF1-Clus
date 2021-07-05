9429552e3b11eedc78faef70ff1b732e8ff97ab8
Elaine Cecília Gatto <elainececiliagatto@gmail.com>
2021-07-05 11:36:13 -0300

    Update microf1.R

--- scripts/microf1.R
+++ scripts/microf1.R
@@ -124,11 +124,11 @@ cat("\nBPC \t nome: ", dataset_name)
 
 
 ##################################################################################################
-cat("\n\nCopy FROM google drive \n")
-destino = paste(FolderRoot, "/datasets/", dataset_name, sep="")
-origem = paste("cloud:elaine/Datasets/CrossValidation_WithValidation/", dataset_name, sep="")
-comando = paste("rclone -v copy ", origem, " ", destino, sep="")
-print(system(comando))
+#cat("\n\nCopy FROM google drive \n")
+#destino = paste(FolderRoot, "/datasets/", dataset_name, sep="")
+#origem = paste("cloud:elaine/Datasets/CrossValidation_WithValidation/", dataset_name, sep="")
+#comando = paste("rclone -v copy ", origem, " ", destino, sep="")
+#print(system(comando))
 
 
 
@@ -154,14 +154,14 @@ diretorios <- directories(dataset_name, folderResults)
 
 
 ##################################################################################################
-cat("\n Copy partitions from google drive")
-destino = paste(diretorios$folderPartitions, "/", dataset_name, sep="")
-if(dir.exists(destino)==FALSE){
-  dir.create(destino)
-}
-origem = paste("cloud:elaine/[2021]ResultadosExperimentos/Generate-Partitions-Random1/", dataset_name, sep="")
-comando1 = paste("rclone -v copy ", origem, " ", destino, sep="")
-print(system(comando1))
+#cat("\n Copy partitions from google drive")
+#destino = paste(diretorios$folderPartitions, "/", dataset_name, sep="")
+#if(dir.exists(destino)==FALSE){
+#  dir.create(destino)
+#}
+#origem = paste("cloud:elaine/[2021]ResultadosExperimentos/Generate-Partitions-Random1/", dataset_name, sep="")
+#comando1 = paste("rclone -v copy ", origem, " ", destino, sep="")
+#print(system(comando1))
 
 
 ##################################################################################################
@@ -202,19 +202,19 @@ print(system(str4))
 
 
 ########################################################################################################################
-cat("\n Copy Results to google drive")
-origem = paste(diretorios$folderDatasetResults, "/", dataset_name, "-results-bpc.tar.gz", sep="")
-destino = paste("cloud:elaine/[2021]ResultadosExperimentos/Best-Partition-Clus/R1/Micro-F1/", dataset_name, sep="")
-comando1 = paste("rclone -v copy ", origem, " ", destino, sep="")
-print(system(comando1))
+#cat("\n Copy Results to google drive")
+#origem = paste(diretorios$folderDatasetResults, "/", dataset_name, "-results-bpc.tar.gz", sep="")
+#destino = paste("cloud:elaine/[2021]ResultadosExperimentos/Best-Partition-Clus/R1/Micro-F1/", dataset_name, sep="")
+#comando1 = paste("rclone -v copy ", origem, " ", destino, sep="")
+#print(system(comando1))
 
 
 ########################################################################################################################
-cat("\n Copy Outupt to google drive")
-origem = diretorios$folderOutputDataset
-destino = paste("cloud:elaine/[2021]ResultadosExperimentos/Best-Partition-Clus/R1/Micro-F1/", dataset_name, sep="")
-comando2 = paste("rclone -v copy ", origem, " ", destino, sep="")
-print(system(comando2))
+#cat("\n Copy Outupt to google drive")
+#origem = diretorios$folderOutputDataset
+#destino = paste("cloud:elaine/[2021]ResultadosExperimentos/Best-Partition-Clus/R1/Micro-F1/", dataset_name, sep="")
+#comando2 = paste("rclone -v copy ", origem, " ", destino, sep="")
+#print(system(comando2))
 
 
 ##################################################################################################
